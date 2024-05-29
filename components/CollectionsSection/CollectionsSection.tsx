637f04baec5f139aa582aa258400ac0cc545a336
Chris Ware <c.p.ware@hotmail.com>
2024-05-28 20:11:08 -0700

    imported collections successfully

--- components/CollectionsSection/CollectionsSection.tsx
+++ components/CollectionsSection/CollectionsSection.tsx
@@ -1,19 +1,37 @@
 import Link from "next/link";
 import styles from "./CollectionsSection.module.css";
 import { getCollections } from "@/lib/actions";
+import Image from "next/image";
+import LayoutWrapper from "../LayoutWrapper/LayoutWrapper";
+import ContentPadding from "../ContentPadding/ContentPadding";
 
 const CollectionsSection = async () => {
   const collections = await getCollections();
 
   return (
-    <div>
-      <h2>Collections Section</h2>
-      <div>
-        {collections.map((collection: CollectionType, index: number) => (
-          <Link href={`/collections/${collection._id}`}></Link>
-        ))}
-      </div>
-    </div>
+    <LayoutWrapper>
+      <ContentPadding>
+        <h2 className={styles.heading}>Collections</h2>
+        <div className={styles.collectionContainer}>
+          {collections.map((collection: CollectionType) => (
+            <div key={collection._id}>
+              <Link
+                href={`/collections/${collection._id}`}
+                className={styles.imgContainer}
+              >
+                <Image
+                  src={collection.image}
+                  alt={collection.title}
+                  fill
+                  className={styles.img}
+                />
+              </Link>
+              <div className={styles.title}>{collection.title}</div>
+            </div>
+          ))}
+        </div>
+      </ContentPadding>
+    </LayoutWrapper>
   );
 };
 export default CollectionsSection;
